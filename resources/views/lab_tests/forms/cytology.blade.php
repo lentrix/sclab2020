9b09d04fe1382a0dba97b41c774bc16a82729402
lentrix <hawkmanlentrix@gmail.com>
2020-12-23 20:02:45 +0800

    cytology updates

--- resources/views/lab_tests/forms/cytology.blade.php
+++ resources/views/lab_tests/forms/cytology.blade.php
@@ -15,18 +15,25 @@
         {!! Form::open(['url'=>"/labtests/results/$labtest->id", 'method'=>'patch']) !!}
         <table class="table table-striped">
             @for($i=1; $i<count($data); $i++)
-            <tr>
-                <td>
-                    {!! Form::label("item[$i]", $data[$i]->name) !!}
-                </td>
-                <td>
-                    @if($data[$i]->result!='1' && $data[$i]->result!='0')
-                        {!! Form::text("item[$i]", $data[$i]->result, ['class'=>'form-control']) !!}
-                    @else
-                        {!! Form::checkbox("item[$i]", 1, $data[$i]->result, ["id"=>"item[$i]"]) !!}
-                    @endif
-                </td>
-            </tr>
+                @if(isset($data[$i]->header))
+                    <tr>
+                        <td colspan="2" style="font-size: 1.2em; font-weight: bold">
+                            {{$data[$i]->header}}
+                        </td>
+                    </tr>
+                @endif
+                <tr>
+                    <td>
+                        {!! Form::label("item[$i]", $data[$i]->name) !!}
+                    </td>
+                    <td>
+                        @if($data[$i]->result!='1' && $data[$i]->result!='0')
+                            {!! Form::text("item[$i]", $data[$i]->result, ['class'=>'form-control']) !!}
+                        @else
+                            {!! Form::checkbox("item[$i]", 1, $data[$i]->result, ["id"=>"item[$i]"]) !!}
+                        @endif
+                    </td>
+                </tr>
             @endfor
         </table>
         <div class="form-group">
